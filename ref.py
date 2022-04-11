1eb64e626e82bb1722bbf6cc3fa23468f6008130
mebdog <barak.morris@colorado.edu>
2022-04-11 17:14:42 -0600

    Example for rk4

--- ref.py
+++ ref.py
@@ -3,50 +3,60 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    ans1=euler(fcn,0,2,10,0.5)
-    ans2=midpoint(fcn,0,2,10,0.5)
-    ans3=modifiedeuler(fcn,0,2,10,0.5)
-    ans4=rungekutta(fcn,0,2,10,0.5)
-    er1=errorcalc(ans1,sol)
-    er2=errorcalc(ans2,sol)
-    er3=errorcalc(ans3,sol)
-    er4=errorcalc(ans4,sol)
-    # Solution Plotting
-    plt.figure("1st Order Method Approximations")
-    plt.title("1st Order Method Approximations")
-    plt.xlabel("t")
-    plt.ylabel("y")
-    plt.plot(ans1[0],ans1[1],label="Euler")
-    plt.plot(ans2[0],ans2[1],label="Midpoint")
-    plt.plot(ans3[0],ans3[1],label="Modified Euler")
-    plt.plot(ans4[0],ans4[1],label="Runge-Kutta")
+    app1 = rk4sys(1,3,10,[4,3],[u1,u2])
+    exa1 = funceval(u1sol, 1, 3, 10)
+    exa2 = funceval(u2sol, 1, 3, 10)
+    plt.plot(app1[0],app1[1],label="apprx")
+    plt.plot(exa1[0],exa1[1],'r+',label="exact 1")
+    plt.plot(exa2[0],exa2[1],'bo',label="exact 2")
     plt.legend()
-    # Error Plotting
-    plt.figure("1st Order Method Errors")
-    plt.title("1st Order Method Errors")
-    plt.xlabel("t")
-    plt.ylabel("Error")
-    plt.semilogy(er1[0],er1[1],label="Euler")
-    plt.semilogy(er2[0],er2[1],label="Midpoint")
-    plt.semilogy(er3[0],er3[1],label="Modified Euler")
-    plt.semilogy(er4[0],er4[1],label="Runge-Kutta")
-    plt.legend()
-    # Stability Plotting
-    plt.figure("Stability for different methods")
-    plt.title("Stability for different methods")
-    plt.xlabel("Re(hk)")
-    plt.ylabel("Im(hk)")
-    x=np.linspace(-2,2,1000)
-    y=np.linspace(-2,2,1000)
-    X,Y=np.meshgrid(x,y)
-    eulerF=(X+1)**2+Y**2-1
-    midpointF=X+(X**2-Y**2)/2
-    plt.contour(X,Y,eulerF,[0])
-    plt.contour(X,Y,midpointF,[0])
-    plt.contour
-    plt.legend()
-
     plt.show()
+    return()
+    
+    # ans1=euler(fcn,0,2,10,0.5)
+    # ans2=midpoint(fcn,0,2,10,0.5)
+    # ans3=modifiedeuler(fcn,0,2,10,0.5)
+    # ans4=rungekutta(fcn,0,2,10,0.5)
+    # er1=errorcalc(ans1,sol)
+    # er2=errorcalc(ans2,sol)
+    # er3=errorcalc(ans3,sol)
+    # er4=errorcalc(ans4,sol)
+    # # Solution Plotting
+    # plt.figure("1st Order Method Approximations")
+    # plt.title("1st Order Method Approximations")
+    # plt.xlabel("t")
+    # plt.ylabel("y")
+    # plt.plot(ans1[0],ans1[1],label="Euler")
+    # plt.plot(ans2[0],ans2[1],label="Midpoint")
+    # plt.plot(ans3[0],ans3[1],label="Modified Euler")
+    # plt.plot(ans4[0],ans4[1],label="Runge-Kutta")
+    # plt.legend()
+    # # Error Plotting
+    # plt.figure("1st Order Method Errors")
+    # plt.title("1st Order Method Errors")
+    # plt.xlabel("t")
+    # plt.ylabel("Error")
+    # plt.semilogy(er1[0],er1[1],label="Euler")
+    # plt.semilogy(er2[0],er2[1],label="Midpoint")
+    # plt.semilogy(er3[0],er3[1],label="Modified Euler")
+    # plt.semilogy(er4[0],er4[1],label="Runge-Kutta")
+    # plt.legend()
+    # # Stability Plotting
+    # plt.figure("Stability for different methods")
+    # plt.title("Stability for different methods")
+    # plt.xlabel("Re(hk)")
+    # plt.ylabel("Im(hk)")
+    # x=np.linspace(-2,2,1000)
+    # y=np.linspace(-2,2,1000)
+    # X,Y=np.meshgrid(x,y)
+    # eulerF=(X+1)**2+Y**2-1
+    # midpointF=X+(X**2-Y**2)/2
+    # plt.contour(X,Y,eulerF,[0])
+    # plt.contour(X,Y,midpointF,[0])
+    # plt.contour
+    # plt.legend()
+
+    # plt.show()
 
 # Solves for y'=f(t,y)
 # a < t < b ; t(0)=alpha
@@ -141,11 +151,11 @@ def rk4sys(a,b,N,alpham,fjtu):
     m = len(alpham)
     h = (b-a)/N
     t = a
-    k1,k2,k3,k4,wd,w = [],[],[],[],[],[],[],[]
+    k1,k2,k3,k4,wd,w,ti,xi = [],[],[],[],[],[],[],[]
     for i in range(m):
         k1.append(0);k2.append(0);k3.append(0);k4.append(0);wd.append(0);w.append(alpham[i])
-    ti=[a]
-    xi=[w]
+    ti.append(t)
+    xi.append(w[:])
     for i in range(1,N+1):
         for j in range(m): wd[j] = w[j]
         for j in range(m): k1[j] = h*fjtu[j](t,*wd)
@@ -156,9 +166,9 @@ def rk4sys(a,b,N,alpham,fjtu):
         for j in range(m): wd[j] = w[j] + k3[j]
         for j in range(m): k4[j] = h*fjtu[j](t+h,*wd)
         for j in range(m): w[j] = w[j]+(k1[j]+2*k2[j]+2*k3[j]+k4[j])/6
-        t = a + round(i*h,3)
+        t = a + i*h
         ti.append(t)
-        xi.append(w)
+        xi.append(w[:])
     return(ti,xi)
 
 
@@ -202,6 +212,11 @@ def fcn(t,y):return(y-t**2+1)
 def sol(t):return(t**2+2*t+1-0.5*math.exp(t))
 def fcn2(y):return(-y)
 def sol2(t):return(math.sin(t))
+
+def u1(t,y1,y2):return(y2)
+def u2(t,y1,y2):return((-1/t)*y2+(4/t**2)*y1-3/t)
+def u1sol(t):return(2*t**2+t+t**-2)
+def u2sol(t):return(4*t+1-2*t**-3)
 
 if __name__ == '__main__':
     main()
