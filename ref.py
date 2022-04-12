a056aff3acc4a47eb55c1d5be6c37f9e24296f8a
mebdog <barak.morris@colorado.edu>
2022-04-11 22:01:45 -0600

    cleanup- everything but newmark works

--- ref.py
+++ ref.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-
     # Simple Harmonic Motion
     # All stable simple harmonic motion
     exa1 = funceval(u1sol, 0, 5, 1000)
@@ -91,11 +90,9 @@ def main():
     plt.semilogy(app4[0],errorcalc(app4[1],exa1[1]),color='pink',label="RK2")
     plt.semilogy(exa1[0],errorcalc(exa1[1],exa1[1]),label="Exact Solution")
     plt.legend(loc='lower left')
-
     # Damped Harmonic Motion
     # All stable damped harmonic motion
     exa1 = funceval(u3sol, 0, 5, 1000)
-    app0 = leapfrog(xpp,0,5,1000,1,0)
     app1 = rk4sys(0,5,1000,[1,0],[u3,u4])
     app2 = euler_2nd(u3,u4,0,5,1000,1,0)
     app3 = modifiedeuler_2nd(u3,u4,0,5,1000,1,0)
@@ -104,7 +101,6 @@ def main():
     plt.title("Damped Harmonic Motion; N=1000")
     plt.xlabel('t')
     plt.ylabel('x')
-    plt.plot(app0[0],app0[1],'g',label="Leapfrog")
     plt.plot(app1[0],np.transpose(app1[1])[0],'b',label="RK4")
     plt.plot(app2[0],app2[1],color="orange",label="Euler")
     plt.plot(app3[0],app3[1],'r',label="Modified Euler")
@@ -115,7 +111,6 @@ def main():
     plt.title("Damped Harmonic Motion Error; N=1000")
     plt.xlabel('t')
     plt.ylabel('Error')
-    plt.semilogy(app0[0],errorcalc(app0[1],exa1[1]),'g',label="Leapfrog")
     plt.semilogy(app1[0],errorcalc(np.transpose(app1[1])[0],exa1[1]),'b',label="RK4 ")
     plt.semilogy(app2[0],errorcalc(app2[1],exa1[1]),color="orange",label="Euler")
     plt.semilogy(app3[0],errorcalc(app3[1],exa1[1]),'r',label="Modified Euler")
@@ -123,7 +118,6 @@ def main():
     plt.legend(loc='lower left')
     # Some stable damped harmonic motion
     exa1 = funceval(u3sol, 0, 5, 100)
-    app0 = leapfrog(xpp,0,5,100,1,0)
     app1 = rk4sys(0,5,100,[1,0],[u3,u4])
     app2 = euler_2nd(u3,u4,0,5,100,1,0)
     app3 = modifiedeuler_2nd(u3,u4,0,5,100,1,0)
@@ -132,7 +126,6 @@ def main():
     plt.title("Damped Harmonic Motion; N=100")
     plt.xlabel('t')
     plt.ylabel('x')
-    plt.plot(app0[0],app0[1],'g',label="Leapfrog")
     plt.plot(app1[0],np.transpose(app1[1])[0],'b',label="RK4")
     plt.plot(app2[0],app2[1],color="orange",label="Euler")
     plt.plot(app3[0],app3[1],'r',label="Modified Euler")
@@ -143,7 +136,6 @@ def main():
     plt.title("Damped Harmonic Motion Error; N=100")
     plt.xlabel('t')
     plt.ylabel('Error')
-    plt.semilogy(app0[0],errorcalc(app0[1],exa1[1]),'g',label="Leapfrog")
     plt.semilogy(app1[0],errorcalc(np.transpose(app1[1])[0],exa1[1]),'b',label="RK4 ")
     plt.semilogy(app2[0],errorcalc(app2[1],exa1[1]),color="orange",label="Euler")
     plt.semilogy(app3[0],errorcalc(app3[1],exa1[1]),'r',label="Modified Euler")
@@ -152,7 +144,6 @@ def main():
     plt.legend(loc='lower left')
     # All unstable damped harmonic motion
     exa1 = funceval(u3sol, 0, 5, 10)
-    # app0 = leapfrog(xpp,0,5,10,1,0)
     app1 = rk4sys(0,5,10,[1,0],[u3,u4])
     app2 = euler_2nd(u3,u4,0,5,10,1,0)
     app3 = modifiedeuler_2nd(u3,u4,0,5,10,1,0)
@@ -161,7 +152,6 @@ def main():
     plt.title("Damped Harmonic Motion; N=10")
     plt.xlabel('t')
     plt.ylabel('x')
-    # plt.plot(app0[0],app0[1],'g',label="Leapfrog")
     plt.plot(app1[0],np.transpose(app1[1])[0],'b',label="RK4")
     plt.plot(app2[0],app2[1],color="orange",label="Euler")
     plt.plot(app3[0],app3[1],'r',label="Modified Euler")
@@ -172,99 +162,15 @@ def main():
     plt.title("Damped Harmonic Motion Error; N=10")
     plt.xlabel('t')
     plt.ylabel('Error')
-    # plt.semilogy(app0[0],errorcalc(app0[1],exa1[1]),'g',label="Leapfrog")
     plt.semilogy(app1[0],errorcalc(np.transpose(app1[1])[0],exa1[1]),'b',label="RK4 ")
     plt.semilogy(app2[0],errorcalc(app2[1],exa1[1]),color="orange",label="Euler")
     plt.semilogy(app3[0],errorcalc(app3[1],exa1[1]),'r',label="Modified Euler")
     plt.semilogy(app4[0],errorcalc(app4[1],exa1[1]),color='pink',label="RK2")
     plt.semilogy(exa1[0],errorcalc(exa1[1],exa1[1]),label="Exact Solution")
     plt.legend(loc='lower left')
-
     plt.show()
     return()
-    
-    # ans1=euler(fcn,0,2,10,0.5)
-    # ans2=midpoint(fcn,0,2,10,0.5)
-    # ans3=modifiedeuler(fcn,0,2,10,0.5)
-    # ans4=rungekutta(fcn,0,2,10,0.5)
-    # er1=errorcalc(ans1,sol)
-    # er2=errorcalc(ans2,sol)
-    # er3=errorcalc(ans3,sol)
-    # er4=errorcalc(ans4,sol)
-    # # Solution Plotting
-    # plt.figure("1st Order Method Approximations")
-    # plt.title("1st Order Method Approximations")
-    # plt.xlabel("t")
-    # plt.ylabel("y")
-    # plt.plot(ans1[0],ans1[1],label="Euler")
-    # plt.plot(ans2[0],ans2[1],label="Midpoint")
-    # plt.plot(ans3[0],ans3[1],label="Modified Euler")
-    # plt.plot(ans4[0],ans4[1],label="Runge-Kutta")
-    # plt.legend()
-    # # Error Plotting
-    # plt.figure("1st Order Method Errors")
-    # plt.title("1st Order Method Errors")
-    # plt.xlabel("t")
-    # plt.ylabel("Error")
-    # plt.semilogy(er1[0],er1[1],label="Euler")
-    # plt.semilogy(er2[0],er2[1],label="Midpoint")
-    # plt.semilogy(er3[0],er3[1],label="Modified Euler")
-    # plt.semilogy(er4[0],er4[1],label="Runge-Kutta")
-    # plt.legend()
-    # # Stability Plotting
-    # plt.figure("Stability for different methods")
-    # plt.title("Stability for different methods")
-    # plt.xlabel("Re(hk)")
-    # plt.ylabel("Im(hk)")
-    # x=np.linspace(-2,2,1000)
-    # y=np.linspace(-2,2,1000)
-    # X,Y=np.meshgrid(x,y)
-    # eulerF=(X+1)**2+Y**2-1
-    # midpointF=X+(X**2-Y**2)/2
-    # plt.contour(X,Y,eulerF,[0])
-    # plt.contour(X,Y,midpointF,[0])
-    # plt.contour
-    # plt.legend()
 
-    # plt.show()
-
-# Solves for y'=f(t,y)
-# a < t < b ; t(0)=alpha
-def euler(f,a,b,N,alpha):
-    h=(b-a)/N
-    t=[a+h*i for i in range(N+1)]
-    w=[alpha for i in range(N+1)]
-    for i in range(1,N+1):
-        w[i] = w[i-1]+h*f(t[i-1],w[i-1])
-    return(t,w)
-def midpoint(f,a,b,N,alpha):
-    h=(b-a)/N
-    t=[a+h*i for i in range(N+1)]
-    w=[alpha for i in range(N+1)]
-    for i in range(1,N+1):
-        w[i] = w[i-1]+h*f(t[i-1]+h/2,w[i-1]+(h/2)*f(t[i-1],w[i-1]))
-    return(t,w)
-def modifiedeuler(f,a,b,N,alpha):
-    h=(b-a)/N
-    t=[a+h*i for i in range(N+1)]
-    w=[alpha for i in range(N+1)]
-    for i in range(1,N+1):
-        w[i] = w[i-1]+(h/2)*(f(t[i-1],w[i-1])+f(t[i],w[i-1]+h*f(t[i-1],w[i-1])))
-    return(t,w)
-def rungekutta(f,a,b,N,alpha):
-    h=(b-a)/N
-    t=[a+h*i for i in range(N+1)]
-    w=[alpha for i in range(N+1)]
-    for i in range(1,N+1):
-        k1=h*f(t[i-1],w[i-1])
-        k2=h*f(t[i-1]+h/2,w[i-1]+k1/2)
-        k3=h*f(t[i-1]+h/2,w[i-1]+k2/2)
-        k4=h*f(t[i],w[i-1]+k3)
-        w[i]=w[i-1]+(1/6)*(k1+2*k2+2*k3+k4)
-    return(t,w)
-
-# Solves for x"=f(x)
-# with Initial conditions x(a) = alpha and x'(a) = beta, a < t < b
 def leapfrog(f,a,b,N,alpha,beta):
     h=(b-a)/N
     t=[a+h*i for i in range(N+1)]
@@ -275,10 +181,7 @@ def leapfrog(f,a,b,N,alpha,beta):
         v[i] = v[i-1]+(1/2)*(f(w[i-1])+f(w[i]))*h
     return(t,w)
 
-# Solving for Mx" + Cx' + Kx = f(t)
-# with x(a) = x0, y(a) = y0
 def euler_2nd(F,G,a,b,N,x0,y0):
-    # substituting x' = y = F(t,x,y) and y' = (f(t) - Cy - Kx)/M = G(t,x,y)
     h=(b-a)/N
     t=[a+h*i for i in range(N+1)]
     x=[x0 for i in range(N+1)]
@@ -308,7 +211,6 @@ def midpoint_2nd(F,G,a,b,N,x0,y0):
         y[i] = y[i-1]+h*G(t[i-1]+h/2,x[i-1],y[i-1]+(h/2)*G(t[i-1],x[i-1],y[i-1]))
     return(t,x)
 
-# Runge kutta fourth order approximation for a system
 def rk4sys(a,b,N,alpham,fjtu):
     m = len(alpham)
     h = (b-a)/N
